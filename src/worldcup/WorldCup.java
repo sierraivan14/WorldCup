e15db4dd5e440a63f0db584db2236fe0c44f89fe
sierraivan14 <gokuv@Sierra>
2026-02-26 20:21:08 -0600

    Adding some try catch, only if the file .txt is not found

--- src/worldcup/WorldCup.java
+++ src/worldcup/WorldCup.java
@@ -1,5 +1,20 @@
 package worldcup;
 
+import java.io.FileNotFoundException;
+import javax.swing.JOptionPane;
+
 public class WorldCup {
+
+    String dataSet;
+
+    WorldCup() {
+        try{
+            dataSet = Archivo.load();
+        }catch(FileNotFoundException e){
+            JOptionPane.showMessageDialog(null, "El archivo de datos no existe", "World Cup" , JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        }
+        
+    }
 
 }
